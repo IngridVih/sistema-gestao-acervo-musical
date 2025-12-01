9abcf69c11c7717d3fce75d770adf4d75b4724ef
Ingrid Vitória <159635392+IngridVih@users.noreply.github.com>
2025-12-01 20:14:57 -0300

    Update sgam_musicdb_completo.sql

--- sgam_musicdb_completo.sql
+++ sgam_musicdb_completo.sql
@@ -165,11 +165,49 @@ WHERE nacionalidade = 'Brasil';
 -- 4. MANIPULAÇÃO (UPDATE / DELETE)
 -- ==========================================================
 SET SQL_SAFE_UPDATES = 0;
--- ALTERAR ano do álbum
+
+-- === UPDATES (Total: 3) ===
+-- 1. Corrigir o ano de lançamento de um álbum
 UPDATE ALBUM
 SET ano_lancamento = 1987
 WHERE titulo = 'Dois';
 
--- DELETAR música (CASCADE já cuida das autorias)
+-- 2. Atualizar a duração de uma música específica
+UPDATE MUSICA 
+SET duracao = '00:04:20' 
+WHERE titulo = 'Come Together';
+
+-- 3. Corrigir a nacionalidade de um artista
+UPDATE ARTISTA 
+SET nacionalidade = 'Inglaterra' 
+WHERE nome = 'The Beatles';
+
+-- ----------------------------------------------------------
+-- VISUALIZAR AS MUDANÇAS DOS UPDATES
+-- ----------------------------------------------------------
+SELECT * FROM ALBUM;   -- Veja o ano mudar para 1987
+SELECT * FROM MUSICA;  -- Veja a duração mudar
+SELECT * FROM ARTISTA; -- Veja a nacionalidade mudar
+
+-- === DELETES (Total: 3) ===
+
+-- 1. Deletar uma música específica
 DELETE FROM MUSICA
 WHERE titulo = 'Eduardo e Mônica';
+
+-- 2. Remover uma participação de um artista em um álbum
+DELETE FROM PARTICIPACAO 
+WHERE id_artista = 1 AND id_album = 1;
+
+-- 3. Remover um artista do sistema (Renato Russo)
+-- Isso apagará automaticamente suas autorias devido ao ON DELETE CASCADE
+DELETE FROM ARTISTA 
+WHERE nome = 'Renato Russo';
+
+-- ----------------------------------------------------------
+-- VISUALIZAR AS MUDANÇAS DOS DELETES
+-- ----------------------------------------------------------
+SELECT * FROM MUSICA;       -- "Eduardo e Mônica" deve ter sumido
+SELECT * FROM PARTICIPACAO; -- A linha (1,1) deve ter sumido
+SELECT * FROM ARTISTA;      -- "Renato Russo" deve ter sumido
+SELECT * FROM AUTORIA;      -- As autorias do Renato (id 3) devem ter sumido (Cascade)
