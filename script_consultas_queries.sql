15ac719f944263a491583f85db0177fa8106a62d
Ingrid Vitória <159635392+IngridVih@users.noreply.github.com>
2025-12-01 19:33:19 -0300

    Update script_consultas_queries.sql

--- script_consultas_queries.sql
+++ script_consultas_queries.sql
@@ -1,15 +1,17 @@
+USE SGAM_MusicDB;
+
 -- ==========================================================
 -- 3. CONSULTAS (SELECT)
 -- ==========================================================
 
--- 3.1 Todas as músicas com seus álbuns
+-- 3.1 Listar todas as músicas e seus respectivos álbuns (JOIN simples)
 SELECT m.titulo AS Musica,
        m.duracao,
        a.titulo AS Album
 FROM MUSICA m
 JOIN ALBUM a ON m.id_album = a.id_album;
 
--- 3.2 Artistas que participaram de Abbey Road
+-- 3.2 Listar quais artistas participaram do álbum 'Abbey Road' (JOIN triplo com filtro)
 SELECT art.nome AS Artista,
        alb.titulo AS Album
 FROM ARTISTA art
@@ -17,9 +19,9 @@ JOIN PARTICIPACAO p ON art.id_artista = p.id_artista
 JOIN ALBUM alb ON p.id_album = alb.id_album
 WHERE alb.titulo = 'Abbey Road';
 
--- 3.3 Todas as autorias
+-- 3.3 Listar todas as músicas e seus autores (JOIN triplo)
 SELECT m.titulo AS Musica,
-       ar.nome AS Artista
+       ar.nome AS Autor
 FROM AUTORIA au
 JOIN ARTISTA ar ON au.id_artista = ar.id_artista
 JOIN MUSICA m ON au.id_musica = m.id_musica;
